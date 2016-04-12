356ff923090063907fb26a5d6cdd54cda48dee2d
khirouji20 <khiro20@wireless-10-147-243-169.public.utexas.edu>
2016-04-11 19:42:08 -0500

    created lists for all basketball stats
    added lists for each basketball stat

--- web_scraping/player_bio_info.py
+++ web_scraping/player_bio_info.py
@@ -7,13 +7,101 @@ from bs4 import BeautifulSoup
 response = urlopen("http://stats.nba.com/stats/leaguedashplayerstats?College=&Conference=&Country=&DateFrom=&DateTo=&Division=&DraftPick=&DraftYear=&GameScope=&GameSegment=&Height=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=PerGame&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&Season=2013-14&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&StarterBench=&TeamID=0&VsConference=&VsDivision=&Weight=")
 string = response.read().decode("utf-8")
 responseJson = json.loads(string)
-print(responseJson['resultSets'][0]['rowSet'])
+
+entireSet = responseJson['resultSets'][0]['rowSet']
+print(entireSet)
 
 
-conn = pymysql.connect(host='127.0.0.1', user='root', passwd='Skater123', db='mysql')
-cur = conn.cursor()
-cur.execute("USE teamct")
+# loop to get all stats lists
+fullNameList = []
+lnameList = []
+fnameList = []
+PPGList = []
+APGList = []
+RPGList = []
+SPGList = []
+BPGList = []
+TOsList = []
+FGMList = []
+FGPList = []
+ThreePMList = []
+ThreePPList = []
+FTMList = []
+FTPList = []
+minList = []
+
+for set in entireSet:  
+  fullName = set[1]
+  fullNameList.append(fullName)
+  firstAndLast = fullName.split()
+  fname = firstAndLast[0]
+  lname = firstAndLast[-1]    
+  fnameList.append(fname)
+  lnameList.append(lname) 
+  
+  points = set[29]
+  PPGList.append(points)
+  
+  assists = set[22]
+  APGList.append(assists)
+  
+  rebounds = set[21]
+  RPGList.append(rebounds)
+  
+  steals = set[24]
+  SPGList.append(steals)
+  
+  blocks = set[25]
+  BPGList.append(blocks)
+  
+  turnovers = set[23]
+  TOsList.append(turnovers)
+  
+  FGM = set[11]
+  FGMList.append(FGM)
+  
+  FGP = set[13]
+  FGPList.append(FGP)
+  
+  ThreePM = set[14]
+  ThreePMList.append(ThreePM)
+  
+  ThreePP = set[16]
+  THreePPList.append(ThreePP)
+  
+  FTM = set[17]
+  FTMList.append(FTM)
+  
+  FTP = set[19]
+  FTPList.append(FTP)
+  
+  minutes = set[10]
+  minList.append(minutes)
+    
+print(lnameList)
+print(fnameList)    
+print(PPGList)
+print(APGList)
+print(RPGList)
+print(SPGList)
+print(BPGList)
+print(TOsList)
+print(FGMList)
+print(FGPList)
+print(ThreePMList)
+print(ThreePPList)
+print(FTMList)
+print(FTPList)
+print(minList)
+
+# now, create rows in database in our format
+
+
+  
+#conn = pymysql.connect(host='127.0.0.1', user='root', passwd='xyz', db='mysql')
+#cur = conn.cursor()
+#cur.execute("USE TeamCT")
 #cur.execute("SELECT * FROM pages WHERE id=1")
 #print(cur.fetchone())
-cur.close()
-conn.close()
+#cur.close()
+#conn.close()
