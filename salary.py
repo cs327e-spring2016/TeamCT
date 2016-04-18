08b9650e4592cb3c4be9c56223e5d0072aceca8f
khirouji20 <khiro20@Ks-MacBook-Pro.local>
2016-04-18 00:48:22 -0500

    tried to add PyMySQL code; logic seems right but MySQL doesn't show updates
    my logic seems to be correct, but when I test out the code, my MySQL
    database doesn’t  update

--- salary.py
+++ salary.py
@@ -22,15 +22,27 @@
 #cur.close()
 #conn.close()
 
+def store(player_id, content):
+  cur.execute("INSERT INTO pages (title, content) VALUES (\"%s\", \"%s\")", (title, content))
+  cur.connection.commit()
+
 #Player Names
 import re
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import pymysql
 numPage2016 = 11
 numPage2015 = 11
 numPage2014 = 10
 name = []
 name2016Array = []
+
+### DONT FORGET TO CHANGE PERSONAL PASSWORD PER USER ###
+conn = pymysql.connect(host='127.0.0.1', unix_socket='/tmp/mysql.sock', user='root', passwd='xyz', db='mysql', charset='utf8')
+cur = conn.cursor()
+cur.execute("USE TeamCT")
+
+fillerCount = 1
 for year in (2014, 2015, 2016):
     for pageNumber in range(1, numPage2016+1):
         html = urlopen("http://espn.go.com/nba/salaries/_/year/" + str(year) + "/page/" + str(pageNumber))
@@ -39,18 +51,40 @@ for year in (2014, 2015, 2016):
         for row in table.find_all('tr'):
             col = row.find_all('td')
             name = col[1].text
-            salary = col[3].text
+            salaryString = col[3].text
             index = name.find(",")
             name = name[:index]
-            if name != "NAM" or salary != "SALARY":
-                array = [name]
+            if name != "NAM" or salaryString != "SALARY":
+                # since player_id cannot be NULL, put fillerCount as filler since cannot be NULL or repeating then later update
+                array = [fillerCount, name]
                 prevYear = year-1
                 array.append(str(prevYear) + "-" + str(year)[2:])
-                salary = salary.replace("$", "")
-                salary = salary.replace(",", "")
-                salaryArray = array.append(salary)
+                salaryString = salaryString.replace("$", "")
+                salaryString = salaryString.replace(",", "")
+                salaryArray = array.append(salaryString)
                 name2016Array.append(array)
+                
+                '''
+                ###
+                # now, insert each row (array) into table 'Salary'
+                if year == 2014:
+                  cur.execute("INSERT INTO Salary (player_id, full_name, season_id, salary) VALUES (\"%s\",\"%s\",\"%s\",\"%s\")", (fillerCount, name, "2013-14", salaryString))
+                if year == 2015:
+                  cur.execute("INSERT INTO Salary (player_id, full_name, season_id, salary) VALUES (\"%s\",\"%s\",\"%s\",\"%s\")", (fillerCount, name, "2014-15", salaryString))
+                if year == 2016:
+                  cur.execute("INSERT INTO Salary (player_id, full_name, season_id, salary) VALUES (\"%s\",\"%s\",\"%s\",\"%s\")", (fillerCount, name, "2015-16", salaryString))
+                ###
+                '''
+                fillerCount = fillerCount + 1
+                
+
 print(name2016Array)
+
+cur.close()
+conn.close()
+
+
+
 
 #Player Salaries
 #import re
