b2da829ec19c1e9912b43b4597ce714fa179143d
Ronly Leung <ronlyleung@yahoo.com>
2016-04-18 23:10:23 -0500

    modified player_shoes.py - fixed brand names and added database insertion code

--- web_scraping/player_shoes.py
+++ web_scraping/player_shoes.py
@@ -9,9 +9,12 @@ conn = pymysql.connect(host='127.0.0.1', user='root', passwd='Skater123', db='my
 cur = conn.cursor()
 cur.execute("USE TeamCT")
 
+# row_array stores all the data to be inputted
+# brands stores the key(brand) => value(pages)
 row_array = []
 brands = {"361":1, "adidas":4, "and1":1, "anta":1, "brandblack":1, "jordan":4, "k1x":1, "li-ning":1, "nike":16, "peak":1, "reebok":1, "spalding":1, "under-armour":1}
 
+# execute
 for brand in brands:
     for pageNumber in range(1, brands[brand]+1):
         print("http://www.nbashoesdb.com/en/search/players/brand/" +brand+ "/page/" + str(pageNumber))
@@ -30,8 +33,19 @@ for brand in brands:
                     row_player.append(name)
                 elif(idx == 5):
                     name = col.find("a").text
-                    row_player.append("Nike")
+                    row_player.append(brand)
                     row_player.append(name)
                     row_array.append(row_player)
                 idx+=1
+
+#results
 print(row_array)
+print(len(row_array) + "total records")
+
+# now, insert each row (array) into table 'shoe_endorsement'
+for entry in row_array:
+    cur.execute("INSERT INTO shoe_endorsement (endorsement_id, season_id, shoe_brand, shoe_model, player_id) VALUES (\"%s\",\"%s\",\"%s\",\"%s\",\"%s\")", (temp, temp, entry[1], entry[2], temp))
+    cur.connection.commit()
+
+cur.close()
+conn.close()
