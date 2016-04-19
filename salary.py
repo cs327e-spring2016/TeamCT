5b0fde767de58118b477913ebcba2e8327e56e95
khirouji20 <khiro20@wireless-10-147-170-72.public.utexas.edu>
2016-04-18 19:44:01 -0500

    changed code to accept name, name (instead of full_name)
    see changes in ‘Salary’ database in MySQL Table definitions

--- salary.py
+++ salary.py
@@ -9,6 +9,7 @@ numPage2015 = 11
 numPage2014 = 10
 name = []
 name2016Array = []
+commandList = []
 
 ### DONT FORGET TO CHANGE PERSONAL PASSWORD PER USER ###
 conn = pymysql.connect(host='127.0.0.1', unix_socket='/tmp/mysql.sock', user='root', passwd='xyz', db='mysql', charset='utf8')
@@ -27,9 +28,14 @@ for year in (2014, 2015, 2016):
             salaryString = col[3].text
             index = name.find(",")
             name = name[:index]
+            
             if name != "NAM" or salaryString != "SALARY":
+                nameList = []
+                nameList.append(name.split())
+                last = nameList[-1][-1]
+                first = nameList[-1][0]
                 # since player_id cannot be NULL, put fillerCount as filler since cannot be NULL or repeating then later update
-                array = [fillerCount, name]
+                array = [fillerCount, last, first]
                 prevYear = year-1
                 array.append(str(prevYear) + "-" + str(year)[2:])
                 salaryString = salaryString.replace("$", "")
@@ -37,23 +43,23 @@ for year in (2014, 2015, 2016):
                 salaryArray = array.append(salaryString)
                 name2016Array.append(array)
                 
-               
-                
+
                 # now, insert each row (array) into table 'Salary'
                 if year == 2014:
-                  cur.execute("INSERT INTO Salary (player_id, full_name, season_id, salary) VALUES (\"%s\",\"%s\",\"%s\",\"%s\")", (fillerCount, name, "2013-14", salaryString))
+                  cur.execute("INSERT INTO Salary (player_id, lname, fname, season_id, salary) VALUES (\"%s\",\"%s\",\"%s\",\"%s\",\"%s\")", (fillerCount, last, first, "2013-14", salaryString))
                   cur.connection.commit()
                 if year == 2015:
-                  cur.execute("INSERT INTO Salary (player_id, full_name, season_id, salary) VALUES (\"%s\",\"%s\",\"%s\",\"%s\")", (fillerCount, name, "2014-15", salaryString))
+                  cur.execute("INSERT INTO Salary (player_id, lname, fname, season_id, salary) VALUES (\"%s\",\"%s\",\"%s\",\"%s\",\"%s\")", (fillerCount, last, first, "2014-15", salaryString))
                   cur.connection.commit()
                 if year == 2016:
-                  cur.execute("INSERT INTO Salary (player_id, full_name, season_id, salary) VALUES (\"%s\",\"%s\",\"%s\",\"%s\")", (fillerCount, name, "2015-16", salaryString))
+                  cur.execute("INSERT INTO Salary (player_id, lname, fname, season_id, salary) VALUES (\"%s\",\"%s\",\"%s\",\"%s\",\"%s\")", (fillerCount, last, first, "2015-16", salaryString))
                   cur.connection.commit()
                 
                 fillerCount = fillerCount + 1
                 
-
+                
 print(name2016Array)
+
 
 cur.close()
 conn.close()
