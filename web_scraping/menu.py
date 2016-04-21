9f301b28788d5449059071b3a5d7313217a33ff8
khirouji20 <khiro20@wireless-10-147-22-117.public.utexas.edu>
2016-04-20 19:59:10 -0500

    main menu formatting

--- web_scraping/menu.py
+++ web_scraping/menu.py
@@ -11,33 +11,43 @@ cur = conn.cursor()
 cur.execute("USE TeamCT")
 
 
+# Main Menu
+##############
 ans = True
 while ans:
   mainMenu = str(input("Choose one of the following menu options(#): \n 1) Colleges \n 2) Salaries \n 3) Player Lookup\n")
 
   if mainMenu == "1":
-
+    print("College")
+    # INSERT collegeMenu code here
   elif mainMenu == "2":
-  
+    print("Salaries")
+    # INSERT salaryMenu code here
   elif mainMenu == "3":
+    print("Player Lookup")
+    # INSERT lookupMenu code here
+  elif mainMenu == "":
+    print("Please Choose a Valid Menu Option")
 
 
 
 '''
-ans = True
-while ans:
+# College Menu
+##############
+ansCollege = True
+while ansCollege:
   collegeMenu = str(input("Choose one of the following numbers to select all NBA players from that college: \n 1) Texas \n 2) Duke \n 3) UNC \n 4) Kansas \n 5) UCLA \n"))
 
   if collegeMenu == "1":
     print("Texas")
     cur.execute("SELECT * FROM Player_Bio_Info WHERE college = "Texas";
     cur.connection.commit()
-    ans = False
+    ansCollege = False
   elif collegeMenu == "2":
     print("Duke")
     cur.execute("SELECT * FROM Player_Bio_Info WHERE college = "Duke";
     cur.connection.commit()
-    ans = False
+    ansCollege = False
   elif collegeMenu == "3":
     print("UNC")
     cur.execute("SELECT * FROM Player_Bio_Info WHERE college = "UNC";
@@ -47,17 +57,19 @@ while ans:
     print("Kansas")
     cur.execute("SELECT * FROM Player_Bio_Info WHERE college = "Kansas";
     cur.connection.commit()
-    ans = False
+    ansCollege = False
   elif collegeMenu == "5":
     print("UCLA")
     cur.execute("SELECT * FROM Player_Bio_Info WHERE college = "UCLA";
     cur.connection.commit()
-    ans = False
+    ansCollege = False
   elif collegeMenu != "":  
     print("Not Valid Choice, Please Try Again")
 '''  
-ans = True
-while ans:
+# Salary Menu
+#################
+ansSalary = True
+while ansSalary:
   salaryMenu = str(input("Choose one of the following numbers to select all NBA players making salary in that range during the 2013-14 season: \n 1) More than $20M \n 2) Between $10M and $20M \n 3) Between $5M and $10M \n 4) Between $1M and $5M \n 5) Less than $1M \n"))
 
   if salaryMenu == "1":
@@ -74,30 +86,41 @@ while ans:
     print(x)
     '''
     
-    ans = False
+    ansSalary = False
   elif salaryMenu == "2":
     print("Between $10M and $20M")
     cur.execute("SELECT lname, fname, salary FROM Salary WHERE season = '2013-14' AND (salary > 10000000 AND salary <= 20000000)")
     cur.connection.commit()
-    ans = False
+    ansSalary = False
   elif salaryMenu == "3":
     print("Between $5M and $10M")
     cur.execute("SELECT lname, fname, salary FROM Salary WHERE season = '2013-14' AND (salary > 5000000 AND salary <= 10000000)")
     cur.connection.commit()
-    ans = False
+    ansSalary = False
   elif salaryMenu == "4":
     print("Between $1M and $5M")
     cur.execute("SELECT lname, fname, salary FROM Salary WHERE season = '2013-14' AND (salary > 1000000 AND salary <= 5000000)")
     cur.connection.commit()
-    ans = False
+    ansSalary = False
   elif salaryMenu == "5":
     print("Less than $1M")
     cur.execute("SELECT lname, fname, salary FROM Salary WHERE season = '2013-14' AND  salary <= 1000000)")
     cur.connection.commit()
-    ans = False
-  elif collegeMenu != "":  
+    ansSalary = False
+  elif salaryMenu != "":  
     print("Not Valid Choice, Please Try Again")
 
-
+# Player Lookup Menu
+##############
+ansLookup = True
+while ansLookup:
+  print("This lookup will find the basketball stats of a desired player in a specific season.")
+  lastName = str(input("Last Name: ")
+  firstName = str(input("First Name: ")
+  years = str(input("Which season: ")  
+  
+  #....
+  #....
+  
 cur.close()
 conn.close()
