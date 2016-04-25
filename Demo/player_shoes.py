c363216b52d651bd52b9af861700871ee9dffec1
Steven Tran <stktran@gmail.com>
2016-04-25 10:30:50 -0500

    Made a new Demo folder to use when we Demo

---
+++ Demo/player_shoes.py
@@ -0,0 +1,65 @@
+import pymysql
+from urllib.request import urlopen, Request
+from bs4 import BeautifulSoup
+import re
+
+
+### DONT FORGET TO CHANGE PERSONAL PASSWORD PER USER ###
+conn = pymysql.connect(host='127.0.0.1', user='root', passwd='skt522', db='mysql', charset='utf8')
+cur = conn.cursor()
+cur.execute("USE TeamCT")
+
+# row_array stores all the data to be inputted
+# brands stores the key(brand) => value(pages)
+row_array = []
+brands = {"361":1, "adidas":4, "and1":1, "anta":1, "brandblack":1, "jordan":4, "k1x":1, "li-ning":1, "nike":16, "peak":1, "reebok":1, "spalding":1, "under-armour":1}
+
+# execute
+for brand in brands:
+    for pageNumber in range(1, brands[brand]+1):
+        print("http://www.nbashoesdb.com/en/search/players/brand/" +brand+ "/page/" + str(pageNumber))
+        html = Request("http://www.nbashoesdb.com/en/search/players/brand/" +brand+ "/page/" + str(pageNumber), headers={'User-Agent': 'Mozilla/5.0'})
+        bsObj = BeautifulSoup(urlopen(html).read(), 'html.parser')
+        table = bsObj.find('table')
+        for row in table.find_all('tr'):
+            idx = 0
+            row_player = []
+            for col in row.find_all('td'):
+                if(idx == 1):
+                    name = col.find("a").text
+                    name = name.replace("\\", "")
+                    name = name.split()
+                    if(len(name) != 1):
+                        row_player.append(name[1])
+                        row_player.append(name[0])
+                    elif(name == "Nene"):
+                        row_player.append("Hilario")
+                        row_player.append("Nene")
+                elif(idx == 5):
+                    name = col.find("a").text
+                    row_player.append(brand)
+                    row_player.append(name)
+                    row_array.append(row_player)
+                idx+=1
+
+#results
+print(row_array)
+print(str(len(row_array)) + "total records")
+
+# now, insert each row (array) into table 'shoe_endorsement'
+e_id = 1
+for entry in row_array:
+    # get player id
+    cur.execute("SELECT * FROM Player_Bio_Info WHERE lname = %s AND fname = %s", (entry[0], entry[1]))
+    if cur.rowcount:
+        playerID = cur.fetchone()
+        playerID = list(playerID)
+        playerID = playerID[0]
+        cur.execute("INSERT INTO shoe_endorsement (endorsement_id, lname, fname, shoe_brand, shoe_model, player_id) VALUES (%s,%s,%s,%s,%s,%s)", (e_id, entry[0], entry[1], entry[2], entry[3], playerID))
+        cur.connection.commit()
+        e_id+=1
+    else:
+        print("Missing player_id: " + entry[1] + " " + entry[0])
+print(e_id)
+cur.close()
+conn.close()
---
+++ Demo/salary.py
@@ -0,0 +1,74 @@
+
+#Player Names
+import re
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import pymysql
+numPage2016 = 11
+numPage2015 = 11
+numPage2014 = 10
+name = []
+name2016Array = []
+commandList = []
+
+### DONT FORGET TO CHANGE PERSONAL PASSWORD PER USER ###
+conn = pymysql.connect(host='127.0.0.1', user='root', passwd='skt522', db='mysql', charset='utf8')
+cur = conn.cursor()
+cur.execute("USE TeamCT")
+
+salaryID = 1
+
+for year in (2014, 2015, 2016):
+    for pageNumber in range(1, numPage2016+1):
+        html = urlopen("http://espn.go.com/nba/salaries/_/year/" + str(year) + "/page/" + str(pageNumber))
+        bsObj=BeautifulSoup(html.read(), 'html.parser')
+        table = bsObj.find('table')
+        for row in table.find_all('tr'):
+            col = row.find_all('td')
+            name = col[1].text
+            salaryString = col[3].text
+            index = name.find(",")
+            name = name[:index]
+            
+            if name != "NAM" or salaryString != "SALARY":
+                nameList = []
+                nameList.append(name.split())
+                last = nameList[-1][-1]
+                first = nameList[-1][0]
+                # since player_id cannot be NULL, put fillerCount as filler since cannot be NULL or repeating then later update
+                array = [salaryID, last, first]
+                prevYear = year-1
+                array.append(str(prevYear) + "-" + str(year)[2:])
+                salaryString = salaryString.replace("$", "")
+                salaryString = salaryString.replace(",", "")
+                salaryArray = array.append(salaryString)
+                name2016Array.append(array)
+
+                print(last, first)
+                # get player id
+                cur.execute("SELECT * FROM Player_Bio_Info WHERE lname = %s AND fname = %s", (last, first))
+                if cur.rowcount:
+                    playerID = cur.fetchone()
+                    playerID = list(playerID)
+                    playerID = playerID[0]
+
+
+                    # now, insert each row (array) into table 'Salary'
+                    if year == 2014:
+                        cur.execute("INSERT INTO Salary (salary_id, lname, fname, season, salary, player_id) VALUES (%s,%s,%s,%s,%s,%s)", (salaryID, last, first, "2013-14", int(salaryString), int(playerID)))
+                        cur.connection.commit()
+                    if year == 2015:
+                        cur.execute("INSERT INTO Salary (salary_id, lname, fname, season, salary, player_id) VALUES (%s,%s,%s,%s,%s,%s)", (salaryID, last, first, "2014-15", int(salaryString), int(playerID)))
+                        cur.connection.commit()
+                    if year == 2016:
+                        cur.execute("INSERT INTO Salary (salary_id, lname, fname, season, salary, player_id) VALUES (%s,%s,%s,%s,%s,%s)", (salaryID, last, first, "2015-16", int(salaryString), int(playerID)))
+                        cur.connection.commit()
+
+                salaryID = salaryID + 1
+
+#print(name2016Array)
+
+# sort name2016Array
+
+cur.close()
+conn.close()
